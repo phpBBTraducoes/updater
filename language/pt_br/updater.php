5636b71eae911dc898bbd0e56b4c5fc826004b26
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-03-29 23:46:06 -0300

    UPDATE

--- language/pt_br/updater.php
+++ language/pt_br/updater.php
@@ -4,7 +4,7 @@
 * @package Upload Extensions Updater
 * @copyright (c) 2015 - 2019 Igor Lavrov (https://github.com/LavIgor)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* Brazilian Portuguese translation by eunaumtenhoid (c) 2019 [ver 1.1.0] (https://github.com/phpBBTraducoes)
+* Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.1.0] (https://github.com/phpBBTraducoes)
 */
 
 if (!defined('IN_PHPBB'))
